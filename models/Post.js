99a2d87cc5de963b2837fecb66af20d8ecc5a634
Christian Tielking <tielking5@gmail.com>
2021-08-11 22:18:45 -0400

    added post model

---
+++ models/Post.js
@@ -0,0 +1,31 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Post;
