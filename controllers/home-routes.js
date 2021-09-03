5b6ae53c1bfac4d9e09642971599b98921f32ab6
Christian Tielking <tielking5@gmail.com>
2021-09-03 17:25:04 -0400

    comments appear on single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -56,7 +56,59 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/')
+  }
+
   res.render('signup')
+})
+
+router.get('/posts/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_title',
+      'post_content',
+      'created_at'
+    ],
+    include: [
+        {
+            model: Comment,
+            attributes: [
+                'id',
+                'post_id',
+                'user_id',
+                'created_at',
+                'comment_text'
+            ],
+            include:{
+            model: User,
+            attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+  })
+  .then(dbPostData => {
+    const post = dbPostData.get({ plain: true });
+    const loggedIn = req.session.loggedIn
+    console.log(post)
+
+    if (loggedIn) {
+      res.render('single-post', {
+        post,
+        loggedIn
+      })
+    } else {
+      res.render('login')
+    }
+  })
 })
 
 module.exports = router;
